63698f35d1aa55e6c0969c3a22ebe7595cef8f77
Isaac Burgess <is44cb@gmail.com>
2022-12-01 06:12:00 -0800

    Day 1 Challenge Complete

---
+++ Advent_Of_Code/2022/12/01/Solution.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Advent_Of_Code
+{
+    internal static class CalorieTask
+    {
+        public static int? TheMostCarried()
+        {
+            using StreamReader sr = new StreamReader(File.OpenRead(@"..\..\..\2022\12\01\input.txt"));
+            List<int> amounts = new List<int>();
+            string lineStr;
+            int linInt = 0;
+            int current = 0;
+
+            while ((lineStr = sr.ReadLine()) != null)
+            {
+                if ((lineStr.Length > 0) && (int.TryParse(lineStr, out linInt)))
+                {
+                    current += linInt;
+                }
+                else
+                {
+                    amounts.Add(current);
+                    current = 0;
+                }
+            }
+            return amounts.Max();
+        }
+
+        public static int? TopThreeMostCarriedTotal()
+        {
+            StreamReader sr = new StreamReader(File.OpenRead(@"..\..\..\2022\12\01\input.txt"));
+            List<int> amounts = new List<int>();
+            string lineStr;
+            int linInt = 0;
+            int current = 0;
+
+            while ((lineStr = sr.ReadLine()) != null)
+            {
+                if ((lineStr.Length > 0) && (int.TryParse(lineStr, out linInt)))
+                {
+                    current += linInt;
+                }
+                else
+                {
+                    amounts.Add(current);
+                    current = 0;
+                }
+            }
+            //Sort smallest to largest
+            amounts.Sort();
+            //Sort largest to smallest by reversing
+            amounts.Reverse();
+            //Get total of 3-largest amounts
+            return amounts.GetRange(0, 3).Sum();
+        }
+    }
+}
--- Advent_Of_Code/Program.cs
+++ Advent_Of_Code/Program.cs
@@ -1,2 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+Console.WriteLine($"2022-12-01: Part 1:\n" + Advent_Of_Code.CalorieTask.TheMostCarried());
+Console.WriteLine($"2022-12-01: Part 2:\n" + Advent_Of_Code.CalorieTask.TopThreeMostCarriedTotal());
+
+Console.WriteLine("ËND");
