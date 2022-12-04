1f0a2ba79ebb85222ddb0b7ba81a0aae225e4d2d
Isaac Burgess <is44cb@gmail.com>
2022-12-04 04:12:37 -0800

    Day 4 challenge complete

---
+++ Advent_Of_Code/2022/12/04/Solution.cs
@@ -0,0 +1,66 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Advent_Of_Code
+{
+    internal class CampCleanupTask
+    {
+        public static int Overlap1()
+        {
+            using StreamReader sr = new StreamReader(File.OpenRead(@"..\..\..\2022\12\04\input.txt"));
+            int overlapCounter = 0;
+            string line;
+
+            //Read input
+            while ((line = sr.ReadLine()) != null)
+            {
+                string range1 = line[..line.IndexOf(',')];
+                string range2 = line[(line.IndexOf(',')+1)..];
+                int range1start = int.Parse(range1.Split('-')[0]);
+                int range1end = int.Parse(range1.Split('-')[1]);
+                int range2start = int.Parse(range2.Split('-')[0]);
+                int range2end = int.Parse(range2.Split('-')[1]);
+                
+                if ((range1start <= range2start && range1end >= range2end) ||
+                    (range2start <= range1start && range2end >= range1end))
+                {
+                    overlapCounter++;
+                }
+            }
+
+            return overlapCounter;
+        }
+
+        public static int Overlap2()
+        {
+            using StreamReader sr = new StreamReader(File.OpenRead(@"..\..\..\2022\12\04\input.txt"));
+            int overlapCounter = 0;
+            string line;
+
+            //Read input
+            while ((line = sr.ReadLine()) != null)
+            {
+                string range1 = line[..line.IndexOf(',')];
+                string range2 = line[(line.IndexOf(',') + 1)..];
+                int range1start = int.Parse(range1.Split('-')[0]);
+                int range1end = int.Parse(range1.Split('-')[1]);
+                int range2start = int.Parse(range2.Split('-')[0]);
+                int range2end = int.Parse(range2.Split('-')[1]);
+
+                if ((range2start >= range1start && range2start <= range1end) ||
+                    (range1start >= range2start && range1start <= range2end) ||
+                    (range2end >= range1start && range2end <= range1end) ||
+                    (range1end >= range2start && range1end <= range2end))
+                {
+                    overlapCounter++;
+                }
+            }
+
+            return overlapCounter;
+        }
+    }
+}
--- Advent_Of_Code/Program.cs
+++ Advent_Of_Code/Program.cs
@@ -13,4 +13,8 @@ Console.WriteLine($"2022-12-02: Part 2:\n" + Advent_Of_Code.RockPaperScissorsTas
 Console.WriteLine($"2022-12-03: Part 1:\n" + Advent_Of_Code.RucksackTask.Search1());
 Console.WriteLine($"2022-12-03: Part 2:\n" + Advent_Of_Code.RucksackTask.Search2());
 
+//December 4th
+Console.WriteLine($"2022-12-04: Part 1:\n" + Advent_Of_Code.CampCleanupTask.Overlap1());
+Console.WriteLine($"2022-12-04: Part 2:\n" + Advent_Of_Code.CampCleanupTask.Overlap2());
+
 Console.WriteLine("ËND"); 
