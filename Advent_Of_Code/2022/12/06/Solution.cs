e431d224e74ae109805d9dc5b83777cedc94e8e5
Isaac Burgess <is44cb@gmail.com>
2022-12-06 03:16:46 -0800

    Day 6 challenge complete

---
+++ Advent_Of_Code/2022/12/06/Solution.cs
@@ -0,0 +1,67 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Advent_Of_Code.RockPaperScissorsTask;
+
+namespace Advent_Of_Code
+{
+    internal class CommunicatorTask
+    {
+        public static int FindStartOfPacket()
+        {
+            using StreamReader sr = new StreamReader(File.OpenRead(@"..\..\..\2022\12\06\input.txt"));
+
+            //Start of packet indicator: 4 different characters
+
+            //Read input
+            int read;
+            int position = 0;
+            char[] buffer = new char[4];
+            while ((read = sr.Read()) >= 0)
+            {
+                buffer[position%4] = (char)read;
+                position++;
+                if (position >= 4)
+                {
+                    if ((buffer[0] != buffer[1]) && (buffer[0] != buffer[2]) && (buffer[0] != buffer[3]) &&
+                        (buffer[1] != buffer[2]) && (buffer[1] != buffer[3]) && (buffer[2] != buffer[3]))
+                    {
+                        return position;
+                    }
+                }
+            }
+            return position;
+        }
+
+        public static int FindStartOfMessage()
+        {
+            using StreamReader sr = new StreamReader(File.OpenRead(@"..\..\..\2022\12\06\input.txt"));
+
+            //Start of message indicator: 14 different characters
+            int disctinctCharacterLengthTrigger = 14;
+
+            //Read input
+            int read;
+            int position = 0;
+            char[] buffer = new char[disctinctCharacterLengthTrigger];
+            while ((read = sr.Read()) >= 0)
+            {
+                buffer[position % disctinctCharacterLengthTrigger] = (char)read;
+                position++;
+                if (position >= disctinctCharacterLengthTrigger)
+                {
+                    if (buffer.ToList().Distinct().Count() == disctinctCharacterLengthTrigger)
+                    {
+                        return position;
+                    }
+                }
+            }
+            return position;
+        }
+
+
+    }
+}
--- Advent_Of_Code/Program.cs
+++ Advent_Of_Code/Program.cs
@@ -21,4 +21,8 @@ Console.WriteLine($"2022-12-04: Part 2:\n" + Advent_Of_Code.CampCleanupTask.Over
 Console.WriteLine($"2022-12-05: Part 1:\n" + Advent_Of_Code.CraneTask.WhatIsOnTop1());
 Console.WriteLine($"2022-12-05: Part 2:\n" + Advent_Of_Code.CraneTask.WhatIsOnTop2());
 
-Console.WriteLine("ËND"); 
+//December 6th
+Console.WriteLine($"2022-12-06: Part 1:\n" + Advent_Of_Code.CommunicatorTask.FindStartOfPacket());
+Console.WriteLine($"2022-12-06: Part 2:\n" + Advent_Of_Code.CommunicatorTask.FindStartOfMessage());
+
+Console.WriteLine("END"); 
